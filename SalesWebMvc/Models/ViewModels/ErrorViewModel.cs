c9996da473b0839df5db9f49304e26f6379c59aa
Your Name <joaoloriato@outlook.com>
2018-12-13 15:12:31 -0200

    Returning custom error page

--- SalesWebMvc/Models/ViewModels/ErrorViewModel.cs
+++ SalesWebMvc/Models/ViewModels/ErrorViewModel.cs
@@ -3,6 +3,9 @@ using System;
 namespace SalesWebMvc.Models.ViewModels {
     public class ErrorViewModel {
         public string RequestId { get; set; }
+        public String Message { get; set; }
+
+
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
